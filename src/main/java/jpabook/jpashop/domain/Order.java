1a5a65bc3652458895fd8c840c75864ee7ce592c
Livelyoneweek <wlvvwhs@naver.com>
2022-07-19 00:16:58 +0900

    0719 엔티티 테이블 완성(연관관계 메소드)

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -19,14 +19,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade=CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade=CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -35,4 +35,21 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status; //주문상태(ORDER,CANCEL)
 
+    // 연관관계 메서드 (키 가진쪽이 메서드갖는게 유리)
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    // 연관관계 메서드 (키 가진쪽이 메서드갖는게 유리)
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    // 연관관계 메서드 (키 가진쪽이 메서드갖는게 유리)
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
